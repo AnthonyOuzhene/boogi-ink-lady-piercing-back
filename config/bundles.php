e4b6157d02de7271133b9a04312861cab60815c2
anthony <a.ouzhene@gmail.com>
2023-01-05 11:41:16 +0100

    installation & parameter Elfinder bundle to upload images with VichUploader

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    FM\ElfinderBundle\FMElfinderBundle::class => ['all' => true],
 ];
