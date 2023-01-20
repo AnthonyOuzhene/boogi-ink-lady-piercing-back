64a4ce66c7e64010b4fccb1c8443208a45c351e0
anthony <a.ouzhene@gmail.com>
2023-01-20 14:42:05 +0100

    serializer on activity entity and deleted it

--- src/Entity/Activity.php
+++ src/Entity/Activity.php
@@ -12,7 +12,7 @@ use Symfony\Component\Serializer\Annotation\Groups;
 /**
  * @ORM\Entity(repositoryClass=ActivityRepository::class)
  * @ApiResource
- * 
+ *  
  */
 
 class Activity
@@ -27,12 +27,12 @@ class Activity
 
     /**
      * @ORM\Column(type="string", length=255)
-     * 
      */
     private $name;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * 
      */
     private $brand_name;
 
